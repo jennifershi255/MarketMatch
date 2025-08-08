7aeb9ae9bdc8f662ad8ba90e87936b11ae1c38b2
jennifershi255 <jennifershi255@gmail.com>
2025-08-07 22:28:00 -0400

    Add explicit OPTIONS preflight handler

--- backend/app.py
+++ backend/app.py
@@ -21,6 +21,15 @@ def after_request(response):
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
     return response
 
+@app.before_request
+def handle_preflight():
+    if request.method == "OPTIONS":
+        response = jsonify({})
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        return response
+
 class MarketMatchAnalyzer:
     def __init__(self):
         self.start_date = '2021-01-01'
